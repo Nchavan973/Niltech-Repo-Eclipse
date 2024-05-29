06e54a49f1513181c8f77463c7d250ea531a492e
Nilesh <nilesh.chavan6080@gmail.com>
2024-05-30 01:10:34 +0530

    TL

--- GIT-Project-01/src/main/java/com/niltech/main/Payment.java
+++ GIT-Project-01/src/main/java/com/niltech/main/Payment.java
@@ -3,11 +3,7 @@ package com.niltech.main;
 public class Payment {
 	
 	public void doPayment() {
-<<<<<<< HEAD
 		System.out.println("Payment Method");
-=======
-		System.out.println("Pay method");
->>>>>>> branch 'master' of https://github.com/Nchavan973/Niltech-Repo-Eclipse.git
 		int amt=1000;
 		int with=500;
 		System.out.println("Transaction Successful.....");
