993229aba5d1b47bf4d3d51d95061298f973ce9e
Nilesh <nilesh.chavan6080@gmail.com>
2024-05-30 00:54:11 +0530

    dev 1 modified paymant method

--- GIT-Project-01/src/main/java/com/niltech/main/Payment.java
+++ GIT-Project-01/src/main/java/com/niltech/main/Payment.java
@@ -3,6 +3,7 @@ package com.niltech.main;
 public class Payment {
 	
 	public void doPayment() {
+		System.out.println("Pay method");
 		int amt=1000;
 		int with=500;
 		System.out.println("Transaction Successful.....");
