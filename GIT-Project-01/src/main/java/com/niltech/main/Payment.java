23e5a6cf2dd7b3ec546497ae45acf3fc0d6684bc
Nilesh <nilesh.chavan6080@gmail.com>
2024-05-30 00:55:28 +0530

    TL committing

--- GIT-Project-01/src/main/java/com/niltech/main/Payment.java
+++ GIT-Project-01/src/main/java/com/niltech/main/Payment.java
@@ -3,6 +3,7 @@ package com.niltech.main;
 public class Payment {
 	
 	public void doPayment() {
+		System.out.println("Payment Method");
 		int amt=1000;
 		int with=500;
 		System.out.println("Transaction Successful.....");
