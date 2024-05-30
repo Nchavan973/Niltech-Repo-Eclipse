32b2411e90c9e31bc6a900587204f80b94451418
Nilesh <nilesh.chavan6080@gmail.com>
2024-05-31 00:11:16 +0530

    bug 456 is fixed

--- GIT-Project-01/src/main/java/com/niltech/delivery/CashOnDelivery.java
+++ GIT-Project-01/src/main/java/com/niltech/delivery/CashOnDelivery.java
@@ -6,4 +6,9 @@ public class CashOnDelivery {
 	public String isDeliyere() {
 		return "yes";
 	}
+	
+	public String reverse(String name) {
+		StringBuffer sb=new StringBuffer(name);
+		return sb.reverse().toString();
+	}
 }
