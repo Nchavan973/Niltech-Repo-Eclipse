d1db30e9dcec94c47fdfff83ae06c33303530368
Nilesh <nilesh.chavan6080@gmail.com>
2024-05-31 00:15:55 +0530

    bug 123 is fixed

--- GIT-Project-01/src/main/java/com/niltech/delivery/CashOnDelivery.java
+++ GIT-Project-01/src/main/java/com/niltech/delivery/CashOnDelivery.java
@@ -7,8 +7,14 @@ public class CashOnDelivery {
 		return "yes";
 	}
 	
+
 	public String reverse(String name) {
 		StringBuffer sb=new StringBuffer(name);
 		return sb.reverse().toString();
+	}
+
+	public int findLength(String name) {
+		System.out.println("Bug 123 is going on...");
+		return name.length();
 	}
 }
